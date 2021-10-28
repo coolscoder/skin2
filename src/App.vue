f116b9656d389cf46e94062677fa5e88ce994fc9
Koichi Noda <wildmountainfox@gmail.com>
2021-10-28 17:53:04 +0900

    feat: add some funcs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div container>
-    <div data-container>
+    <!-- <div data-container>
       <img data-avatar :src="linkdrip.profile.image" />
       <h1 data-username v-text="linkdrip.profile.name.text"></h1>
       <p data-description v-text="linkdrip.profile.description.text"></p>
@@ -15,24 +15,28 @@
           <ExtendedItem v-if="linkdrip.extends[index]" :extends="link.extends" />
         </li>
       </ul>
+    </div> -->
+    <div textborder>
+      <slot v-for="n in 10" :key="n">{{linkdrip.profile.name.text+' &middot;'}}</slot>
     </div>
   </div>
 </template>
 
 <script>
-  import LinkItem from './components/LinkItem.vue'
-  import ExtendedItem from './components/ExtendedItem.vue'
+  // import LinkItem from './components/LinkItem.vue'
+  // import ExtendedItem from './components/ExtendedItem.vue'
 
   export default {
     name: 'App',
-    components: {
-      LinkItem,
-      ExtendedItem,
-    },
+    // components: {
+    //   LinkItem,
+    //   ExtendedItem,
+    // },
     data: () => ({
       search: new URLSearchParams(window.location.search),
       visible: false,
       linktree: false,
+      animate: false,
       linkdrip:{
         skin: 'SKIN_NAME',
         profile:{
@@ -126,6 +130,10 @@
         this.$data.linkdrip.extends.push(false)
       });
     },
+    mounted() {
+      // this.startAnimations();
+      this.startEmblem('[textborder]');
+    },
     methods: {
       handleExtend(index) {
         if(this.$data.linkdrip.extends[index]) {
@@ -133,29 +141,54 @@
         } else {
           this.$data.linkdrip.extends[index] = true
         }
+      },
+      startAnimations() {
+        this.animate = true
+      },
+      startEmblem(el, str) {
+        var element = document.querySelector(el);
+        var text = str ? str : element.innerHTML;
+        element.innerHTML = "";
+        for (var i = 0; i < text.length; i++) {
+          var letter = text[i];
+          var span = document.createElement("span");
+          var node = document.createTextNode(letter);
+          var r = (360 / text.length) * i;
+          var x = (Math.PI / text.length).toFixed(0) * i;
+          var y = (Math.PI / text.length).toFixed(0) * i;
+          span.appendChild(node);
+          span.style.float = "left"
+          span.style.webkitTransform =
+            "rotateZ(" + r + "deg) translate3d(" + x + "px," + y + "px,0)";
+          span.style.transform =
+            "rotateZ(" + r + "deg) translate3d(" + x + "px," + y + "px,0)";
+          element.appendChild(span);
+        }
       }
-    },
+    }
   }
 </script>
 
 <style lang="scss">
   body {
-    background: #000;
+    background: #fff;
     color: #fff;
     font-family: Helvetica;
     text-align: center;
+    margin: 0;
   }
   [container] {
-    padding: 10px;
     background-color: #fff;
     border-radius: 20px;
+    color: black;
   }
 	[data-container] {
-		margin: 10px;
 		padding: 0 1rem;
+    margin: 1rem;
     background-color: #000;
     border: 1px solid white;
     border-radius: 20px;
+    color: #fff;
 	}
 	[data-avatar]{
 		position: relative;
@@ -208,4 +241,38 @@
 			}
 		}
   }
+  [textborder] {
+    position: absolute;
+    width: calc(100vw - 10px);
+    height: calc(100vh -10px);
+    padding: 5px;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%,-50%,0) rotateZ(0);
+    border-radius: 10px;
+    /* animation: spinZ 20s linear infinite; */
+    text-align: left;
+
+    span {
+      position: absolute;
+      display: inline-block;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      text-transform: uppercase;
+      font-family: courier, helvetica, arial, sans-serif;
+      transition: all 0.5s cubic-bezier(0, 0, 0, 1);
+      color: #000;
+    }
+
+    @keyframes spinZ {
+      0% {
+        transform: translate3d(-50%,-50%,0) rotateZ(360deg);
+      }
+      100% {
+        transform:  translate3d(-50%,-50%,0) rotateZ(0deg);
+      }
+    }
+	}
 </style>
